372e4bf766ef609a2ca4bd7c18104b18dd0c3714
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-02-19 16:40:22 +0100

    recept div

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 let url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=cdd151db1b734bc2bb7f450c90fc89ba&query=';
+let button= document.querySelector('#search')
+let inputValue= document.querySelector('#inputValue');
+let Main=document.querySelector('.theMain')
 
-document.getElementById("search").onclick = function(){
+button.onclick = function(){
     search()
 }
 
 function getQuery(){
-    return url + document.getElementById("inputValue").value;
+    return url + inputValue.value;
 }
 
 function compileQuery(url, type, text){
@@ -13,13 +16,34 @@ function compileQuery(url, type, text){
 }
 
 async function search(){
+    let värdet= inputValue.value;
+
     let query = getQuery();
     let respone = await fetch(query);
     if(respone.ok){
         const jsonRespone = await respone.json();
         console.log(jsonRespone);
+
+        let förstaDivFörAllaRecept= document.createElement("div");
+        förstaDivFörAllaRecept.className = "förstaDivFörAllaRecept";
+
+
+      let allaRecept= document.createElement("div");
+      allaRecept.className = "allaRecept";
+
+      let rubrikFörInput = document.createElement("h1");
+      rubrikFörInput.id = "rubrikFörInput";
+      rubrikFörInput.innerHTML = "'"+värdet+"'";
+
+      allaRecept.appendChild(rubrikFörInput);
+      förstaDivFörAllaRecept.appendChild(allaRecept);
+      Main.appendChild(förstaDivFörAllaRecept);
+
     }
     else if(respone.error){
         alert("Failed");
     }
 }
+
+
+
