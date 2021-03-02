768360cf95771391d1087f965cfc667bcd90ad12
Lucas Altesson <lucas.altesson@learnet.se>
2021-03-02 21:08:42 +0100

    filter searches now works

--- code.js
+++ code.js
@@ -1,19 +1,20 @@
-let url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=cdd151db1b734bc2bb7f450c90fc89ba&query=';
+let url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=cdd151db1b734bc2bb7f450c90fc89ba';
 let button= document.querySelector('#button')
 let inputValue= document.querySelector('#inputValue');
-let Main=document.querySelector('.theMain_forRecept')
+let Main=document.querySelector('.theMain_forRecept');
+let intolerances = [];
+    let diets = [];
+    let meals = [];
 
 button.onclick = function(){
     search()
 }
 
 function getQuery(){
-    if(inputValue.value.length > 0)
-    return url + inputValue.value;
-    else{
-        alert("Must search for something");
-    }
+    if(inputValue.value.length > 0) return compileQuery(url, "query", inputValue.value);
+    else return url
 }
+
 
 function compileQuery(url, type, text){
     return `${url}&${type}=${text}`;
@@ -22,6 +23,15 @@ function compileQuery(url, type, text){
 async function search(){
 
     let query = getQuery();
+    
+    if(intolerances.length > 0) query = compileQuery(query, "intolerances", intolerances.toString());
+
+    if(diets.length > 0) query = compileQuery(query, "diet", diets.toString());
+    
+    if(meals.length > 0) query = compileQuery(query, "type", meals.toString());
+
+    console.log(query)
+
     let response;
     try{
         response = await fetch(query);
@@ -33,6 +43,7 @@ async function search(){
     if(response.ok){
             const jsonRespone = await response.json();
             console.log(jsonRespone.results.length);
+            Main.innerHTML = "";
 
         if(jsonRespone.results.length > 0){
             for (let foodObj = 0; foodObj < jsonRespone.results.length; foodObj++) {
@@ -73,11 +84,37 @@ async function search(){
 
 
 $(function(){
+
+
     $('.items').click(function(){
         const value = $(this.parentElement.childNodes[1]).css("border-radius");
             if(value == "20px") $(this.parentElement.childNodes[1]).css("border-radius", "20px 20px 0px 0px");
             else $(this.parentElement.childNodes[1]).css("border-radius", "20px");
         $("ul", this.parentElement).slideToggle(100);
+    })
+
+    $('#Diet').on('click', 'input', function(){
+        if(!this.checked){
+            const index = diets.indexOf(this.value);
+            diets.splice(index, 1);
+        }
+        else diets.push(this.value);
+    })
+
+    $('#Intolerance').on('click', 'input', function(){
+        if(!this.checked){
+            const index = diets.indexOf(this.value);
+            intolerances.splice(index, 1);
+        }
+        else intolerances.push(this.value);
+    })
+
+    $('#Meal').on('click', 'input', function(){
+        if(!this.checked){
+            const index = diets.indexOf(this.value);
+            meals.splice(index, 1);
+        }
+        else meals.push(this.value);
     })
 })
 
