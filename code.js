f2c205f7500097f4dc02de698c4dd817434a37d5
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-03-04 16:02:43 +0100

    remove

--- code.js
+++ code.js
@@ -240,26 +240,6 @@ function callIngredientApi(callURL){
  }
 
 
- function removeElement() {
-  // tar bort element när man kallar på funktionen
-  let removeRecipeDiv = document.querySelectorAll("#recipediv");
-  for (let index = 0; index < removeRecipeDiv.length; index++) {
-    removeRecipeDiv[index].remove();
-  }
-  let removeIngredientsDiv = document.querySelectorAll("#ingredients");
-  for (let index = 0; index < removeIngredientsDiv.length; index++) {
-    removeIngredientsDiv[index].remove();
-  }
-  let removeheader = document.querySelectorAll("h3");
-
-  for (let index = 0; index < removeheader.length; index++) {
-    removeheader[index].remove();
-  }
-}
- 
- 
-
-
 $(function(){
 
     $('.items').click(function(){
